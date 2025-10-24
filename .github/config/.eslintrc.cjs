fd535d4ff0bb8d330d3ede75eb248aff6efecd86
MartinCalamel <martin.calamel@cpe.fr>
2025-10-24 09:18:19 +0200

    Rename ESLint config from JSON to CJS format

--- .github/config/.eslintrc.cjs
+++ .github/config/.eslintrc.cjs
@@ -1,13 +1,14 @@
-{
-  "env": {
-    "browser": true,
-    "es2021": true
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
   },
-  "extends": "eslint:recommended",
-  "rules": {
+  extends: "eslint:recommended",
+  rules: {
     "no-unused-vars": "warn",
     "no-undef": "error",
     "semi": ["warn", "always"],
     "quotes": ["warn", "double"]
   }
-}
+};
+
