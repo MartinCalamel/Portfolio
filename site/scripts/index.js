a141b96a28c63f3ac4d52ddaf21246c8c30d838d
MartinCalamel <martin.calamel@cpe.fr>
2025-11-08 17:28:52 +0100

    add to main.js some fonction to set the localStorage, add the data in the JSON, modify the html for the template.

---
+++ site/scripts/index.js
@@ -0,0 +1,14 @@
+/* index.js
+* Auteur : Martin Calamel
+* Script de la page index.html
+* Fonctions à réaliser: 
+*       * récupérer les données du json.
+*       * Mettre à jour l'ensemble des templates
+* Fonction importées :
+*      * from main.js => get_data_from_json, get_data_from_localStorage, set_up_localStorage()
+*/
+
+function update_template_education() {
+    data = get_data_from_localStorage("education");
+
+}
