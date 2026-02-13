10bf206f65d760a362e8bf7dd0b7d24d584fe39f
MartinCalamel <martin.calamel@cpe.fr>
2026-02-13 19:43:08 +0100

    add animation contact

---
+++ site/scripts/contact.js
@@ -0,0 +1,10 @@
+let photo = document.getElementById("image");
+
+function over(){
+    photo.src = "../images/Contact/profile.jpg";
+    console.log("hey");
+}
+
+function off() {
+        photo.src = "../images/Contact/profile_sketch.jpg";
+    }   
