de0493fd685e5e36fab12d9ffb122e94da49af1c
MartinCalamel <martin.calamel@cpe.fr>
2026-02-17 22:39:24 +0100

    ajout des animation projet

--- site/scripts/projets_img.js
+++ site/scripts/projets_img.js
@@ -7,3 +7,25 @@ function off(id){
     image = document.getElementById(id);
     image.src = "../images/projet/"+id;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        initAnimations();
+    }, 500); // 50ms suffit
+});
+
+function initAnimations() {
+    const elements = document.querySelectorAll('.div-projet');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle('animate', entry.isIntersecting);
+        });
+    }, {
+        threshold: 0.2
+    });
+
+    elements.forEach(el => observer.observe(el));
+}
+
+
